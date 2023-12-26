8f5a61e8a88dc5f4c81d276b0d6ff9dc95c838eb
RsuEduard <rusu00eduard@proton.me>
2023-12-26 21:49:56 +0200

    feat: Base64 decoding

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 use wasm_bindgen::prelude::wasm_bindgen;
 use web_sys::console::log_1 as log;
+use base64::{Engine as _, engine::{self, general_purpose}};
 
 #[wasm_bindgen]
 pub fn grayscale(encoded_file: &str) {
-    log(&encoded_file.into())
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = general_purpose::STANDARD.decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
 }

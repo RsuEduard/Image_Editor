687d5f46a72899c8fb9abcc253056d9c0609096b
RsuEduard <rusu00eduard@proton.me>
2023-12-26 22:14:06 +0200

    feat: encoded the image back to base 64

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use image::load_from_memory;
 use  image::ImageOutputFormat::Png;
 
 #[wasm_bindgen]
-pub fn grayscale(encoded_file: &str) {
+pub fn grayscale(encoded_file: &str) -> String {
     log(&"Grayscale called".into());
 
     let base64_to_vector = general_purpose::STANDARD.decode(encoded_file).unwrap();
@@ -15,11 +15,17 @@ pub fn grayscale(encoded_file: &str) {
     log(&"Image loaded".into());
 
     img = img.grayscale();
-
     log(&"Grayscale effect applied".into());
 
     let mut buffer = vec![];
     img.write_to(&mut buffer, Png).unwrap();
-
     log(&"New image written".into());
+
+    let encoded_img = general_purpose::STANDARD.encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
 }

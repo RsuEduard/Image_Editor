fdf54dfee5fcc607bc8f82f116d320699ccf6948
RsuEduard <rusu00eduard@proton.me>
2023-12-26 21:40:39 +0200

    feat: passed the image from js to rust

--- public/main.js
+++ public/main.js
@@ -8,8 +8,6 @@ async function init() {
     return;
   }
   
-  console.log(rustApp);
-
   const input = document.getElementById("upload");
   const fileReader = new FileReader();
 
@@ -17,8 +15,8 @@ async function init() {
     const base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/, ''
     );
-    console.log(input.files[0]);
-    console.log(base64);
+
+    rustApp.grayscale(base64);
   };
 
   input.addEventListener("change", () => {
